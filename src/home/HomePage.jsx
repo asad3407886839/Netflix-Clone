0a4debdfcee163610054cc94c5520d2dca086a55
asad3407886839 <asad3407886839@gmail.com>
2026-02-11 03:49:19 -0800

    update code

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,21 +1,31 @@
-import React, { useState } from "react"
-import Homes from "../components/homes/Homes"
-import Trending from "../components/trending/Trending"
-import Upcomming from "../components/upcoming/Upcomming"
-import { latest, recommended, upcome } from "../dummyData"
+// import React, { useState } from "react"
+// import Homes from "../components/homes/Homes"
+// import Trending from "../components/trending/Trending"
+// import Upcomming from "../components/upcoming/Upcomming"
+// import { latest, recommended, upcome } from "../dummyData"
+
+// const HomePage = () => {
+//   const [items, setItems] = useState(upcome)
+//   const [item, setItem] = useState(latest)
+//   const [rec, setRec] = useState(recommended)
+//   return (
+//     <>
+//       <Homes />
+//       <Upcomming items={items} title='Upcomming Movies' />
+//       <Upcomming items={item} title='Latest Movies' />
+//       <Trending />
+//       <Upcomming items={rec} title='Recommended Movies' />
+//     </>
+//   )
+// }
+
+// export default HomePage
+
+import React from 'react'
 
 const HomePage = () => {
-  const [items, setItems] = useState(upcome)
-  const [item, setItem] = useState(latest)
-  const [rec, setRec] = useState(recommended)
   return (
-    <>
-      <Homes />
-      <Upcomming items={items} title='Upcomming Movies' />
-      <Upcomming items={item} title='Latest Movies' />
-      <Trending />
-      <Upcomming items={rec} title='Recommended Movies' />
-    </>
+    <div>HomePage</div>
   )
 }
 
